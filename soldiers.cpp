7cf4d6f7ef2d7c588130e70ecbdc601db0897d03
ramnaresh8 <ramnareshkushwah823@gmail.com>
2022-01-09 21:25:06 +0530

    adding a cpp file

---
+++ soldiers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, n, w , sum=0;
+    
+    cin>>k>>n>>w;
+    
+    for(int i=1; i<=w; i++)
+    {
+        sum = sum + k*i;
+    }
+    
+    if(sum<n)
+    return 0;
+    
+    else
+    cout<<sum - n;
+    
+}
